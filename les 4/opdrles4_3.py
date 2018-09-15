4899329691ed51527eb6dfa4af42b8efcb90b128
mejmz <thom.pestman@student.hu.nl>
2018-09-15 16:00:18 +0200

    opdracht 3 voor les 4 van programmeren

---
+++ les 4/opdrles4_3.py
@@ -0,0 +1,10 @@
+
+uurloon = input('Wat verdien je per uur: ')
+uurgewerkt = input('Hoeveel uur heb je gewerkt: ')
+
+salaris = float(uurloon) * int(uurgewerkt)
+
+print(uurgewerkt + ' uur werken levert ' + str(salaris) + ' Euro op')
+
+
+
